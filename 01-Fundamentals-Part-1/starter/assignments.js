d46d752f5cb9ce8f9e92a440efe2d016949473b5
amr9 <amr12201147@gmail.com>
2025-12-15 23:12:51 +0200

    Refactor assignments.js to clarify type conversion examples and add coding challenge implementations in codingChallenege.js

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -63,9 +63,9 @@ else {
 
 // Assignment: Type Conversion and Coercion
 console.log('9' - '5'); // -> 4
-console.log('19' - '13' + '17'); // -> ?
-console.log('19' - '13' + 17); // -> ?
-console.log('123' < 57); // -> ?
+console.log('19' - '13' + '17'); // -> 617
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> False
 console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
 
 
--- 01-Fundamentals-Part-1/starter/codingChallenege.js
+++ 01-Fundamentals-Part-1/starter/codingChallenege.js
@@ -1,3 +1,4 @@
+// Coding Challenge #1
 const markWeight1 = 78;
 const markHeight1 = 1.69;
 
@@ -34,15 +35,31 @@ console.log("Mark has higer BMI than jhon test 1? ", markHigerBMI1);
 
 console.log("Mark has higer BMI than jhon test 2? ", markHigerBMI2);
 
+// Coding Challenge #2
 
+if (BMIMark1 > BMIJohn1) {
+    console.log(`Mark's BMI (${BMIMark1}) is higher than John's (${BMIJohn1})!`);
+}
+else {
+    console.log(`John's BMI (${BMIJohn1}) is higher than Mark's (${BMIMark1})!`);
+}
 
+// Coding Challenge #3
 
+const dolphinScore1 = (96 + 108 + 89) / 3;
+const koalaScore1 = (88 + 91 + 110) / 3;
 
+if (dolphinScore1 > koalaScore1) {
+    console.log(`Dolphin win the trophy with score ${dolphinScore1}`);
+}
+else if (koalaScore1 > dolphinScore1) {
+    console.log(`Koala win the trophy with score ${koalaScore1}`);
+}
+else {
+    console.log(`Both win the trophy with score Dolphin: ${dolphinScore1}  Koala: ${koalaScore1}`);
+}
 
-
-
-
-
+//coding Challenge #4
 
 
 
