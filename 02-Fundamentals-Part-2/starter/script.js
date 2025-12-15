f1a02a4422c9b392c1fcc48dcc97421a60c4460a
amr9 <amr12201147@gmail.com>
2025-12-16 00:06:04 +0200

    Add initial implementation of driving license logic in script.js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';   // Strict Mode is a way to opt in to a restricted variant of JavaScript, 
+// thereby implicitly opting-out of "sloppy mode".
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
