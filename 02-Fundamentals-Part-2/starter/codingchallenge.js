4c85b1582d188f9252bc5cff41967d09d1afd184
amr9 <amr12201147@gmail.com>
2025-12-22 01:36:33 +0200

    Add BMI calculation for Mark and John; compare and log results

--- 02-Fundamentals-Part-2/starter/codingchallenge.js
+++ 02-Fundamentals-Part-2/starter/codingchallenge.js
@@ -37,5 +37,31 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
 
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
 
+//coding Challenge #3
 
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
 
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height)
+        return this.bmi
+    }
+}
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height)
+        return this.bmi
+    }
+}
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higer than ${mark.fullName}'s (${mark.bmi})!`)
+}
+else if (mark.calcBMI() > john.calcBMI()) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higer than ${john.fullName}'s (${john.bmi})!`)
+}
