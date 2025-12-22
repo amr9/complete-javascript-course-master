8432be058d01997cc56f9d4838d4423e1de461e6
amr9 <amr12201147@gmail.com>
2025-12-22 03:20:16 +0200

    Add loop to log weight lifting repetitions in script.js

--- 02-Fundamentals-Part-2/starter/codingchallenge.js
+++ 02-Fundamentals-Part-2/starter/codingchallenge.js
@@ -1,67 +1,99 @@
 "use strict";
 
-// Coding Challenge #1
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// // Coding Challenge #1
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-const scoreDolphins = calcAverage();
-const scoreKoalas = calcAverage();
+// const scoreDolphins = calcAverage();
+// const scoreKoalas = calcAverage();
 
-function checkWinner(avgDolphins, avgKoalas) {
-    if (avgDolphins > avgKoalas) {
-        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
-    }
-    else if (avgKoalas > avgDolphins) {
-        console.log(`Koalas win (${avgDolphins} vs. ${avgKoalas})`);
-    }
-    else {
-        console.log(`No team wins...`);
-    }
+// function checkWinner(avgDolphins, avgKoalas) {
+//     if (avgDolphins > avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//     }
+//     else if (avgKoalas > avgDolphins) {
+//         console.log(`Koalas win (${avgDolphins} vs. ${avgKoalas})`);
+//     }
+//     else {
+//         console.log(`No team wins...`);
+//     }
+// }
+
+// checkWinner(scoreKoalas, scoreDolphins)
+
+// //coding Challenge #2
+// function calcTip(bill) {
+//     if (bill > 50 && bill < 300) {
+//         return 0.15 * bill
+//     }
+//     else {
+//         return 0.20 * bill
+//     }
+// }
+
+// const bills = [125, 555, 44]
+
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
+// //coding Challenge #3
+
+// const mark = {
+//     fullName: "Mark Miller",
+//     mass: 78,
+//     height: 1.69,
+
+//     calcBMI: function () {
+//         this.bmi = this.mass / (this.height * this.height)
+//         return this.bmi
+//     }
+// }
+// const john = {
+//     fullName: "John Smith",
+//     mass: 92,
+//     height: 1.95,
+
+//     calcBMI: function () {
+//         this.bmi = this.mass / (this.height * this.height)
+//         return this.bmi
+//     }
+// }
+// if (john.calcBMI() > mark.calcBMI()) {
+//     console.log(`${john.fullName}'s BMI (${john.bmi}) is higer than ${mark.fullName}'s (${mark.bmi})!`)
+// }
+// else if (mark.calcBMI() > john.calcBMI()) {
+//     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higer than ${john.fullName}'s (${john.bmi})!`)
+// }
+
+
+//coding Challenge #4
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-checkWinner(scoreKoalas, scoreDolphins)
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
 
-//coding Challenge #2
-function calcTip(bill) {
-    if (bill > 50 && bill < 300) {
-        return 0.15 * bill
+const tips = []
+const totals = []
+let index = 0
+
+while (index < 10) {
+    tips.push(calcTip(bills[index]))
+    totals.push(tips[index] + bills[index])
+    index++
+}
+console.log(tips + "  " + totals)
+
+function calcAverage(arr) {
+    let sum = 0
+    let index = 0
+    while (arr) {
+        sum += arr[index]
+        index++
     }
-    else {
-        return 0.20 * bill
-    }
+    return sum / arr.length
 }
 
-const bills = [125, 555, 44]
-
-
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
-
-//coding Challenge #3
-
-const mark = {
-    fullName: "Mark Miller",
-    mass: 78,
-    height: 1.69,
-
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height * this.height)
-        return this.bmi
-    }
-}
-const john = {
-    fullName: "John Smith",
-    mass: 92,
-    height: 1.95,
-
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height * this.height)
-        return this.bmi
-    }
-}
-if (john.calcBMI() > mark.calcBMI()) {
-    console.log(`${john.fullName}'s BMI (${john.bmi}) is higer than ${mark.fullName}'s (${mark.bmi})!`)
-}
-else if (mark.calcBMI() > john.calcBMI()) {
-    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higer than ${john.fullName}'s (${john.bmi})!`)
-}
+const averageTotals = calcAverage(totals)
+console.log(averageTotals)
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -101,6 +101,16 @@ amrObject.getSummary = function () {
 console.log(amrObject.getSummary());
 
 
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
+
+
+
+
+
+
+
 
 
 
