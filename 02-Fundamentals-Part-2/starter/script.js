aa59a65d8ff6398863b87987ed2be8ae7a6a9a7e
amr9 <amr12201147@gmail.com>
2025-12-24 00:06:53 +0200

    Refactor script.js for improved code formatting and readability; adjust indentation and spacing

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';   // Strict Mode is a way to opt in to a restricted variant of JavaScript, 
+"use strict"; // Strict Mode is a way to opt in to a restricted variant of JavaScript,
 // thereby implicitly opting-out of "sloppy mode".
 
 let hasDriversLicense = false;
@@ -11,65 +11,56 @@ if (hasDriversLicense) console.log("I can drive :D");
 // const interface = "Audio";
 // const private = 534;
 
-
-
-
 function logger() {
-    console.log("My name is amr");
+  console.log("My name is amr");
 }
 
-logger()
+logger();
 
 function fruitProcesssor(fruit1, fruit2) {
-    console.log(fruit1, fruit2)
-    const juice = `juice with ${fruit1} and ${fruit2}`
-    return juice
+  console.log(fruit1, fruit2);
+  const juice = `juice with ${fruit1} and ${fruit2}`;
+  return juice;
 }
 
-
-
-let z = (fruitProcesssor("apple", "orange"))
+let z = fruitProcesssor("apple", "orange");
 // console.log(z)
 
-
-const age1 = calcAge(2000)
+const age1 = calcAge(2000);
 
 function calcAge(birthYear) {
-    return 2025 - birthYear
+  return 2025 - birthYear;
 }
-
 
 // console.log(age1)
 
 const calcAge2 = function (birthYear) {
-    return 2025 - birthYear
-}
+  return 2025 - birthYear;
+};
 
 // console.log(calcAge2(1991))
 
-
-const friends = ["Michael", "Steven", "Peter"]
-console.log(friends[0])
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends[0]);
 
 const years = new Array(1991, 1984, 2008, 2020);
-console.log(years.length)
-
+console.log(years.length);
 
 const amrObject = {
-    firstName: "Amr",
-    lastName: "Emad",
-    // age: 2025 - 2000,
-    birthYear: 2000,
-    friends: ["Michael", "Steven", "Peter"]
+  firstName: "Amr",
+  lastName: "Emad",
+  // age: 2025 - 2000,
+  birthYear: 2000,
+  friends: ["Michael", "Steven", "Peter"],
 };
 
 console.log(amrObject);
 
 console.log(amrObject.lastName);
-console.log(amrObject['lastName']);
+console.log(amrObject["lastName"]);
 
-const nameKey = 'Name';
-console.log(amrObject['first' + nameKey]);
+const nameKey = "Name";
+console.log(amrObject["first" + nameKey]);
 
 // const intrestedIN = prompt(`What do you want to know about Amr? Choose between firstName, lastName, age, and friends`);
 
@@ -85,8 +76,8 @@ console.log(`${amrObject.firstName} has ${amrObject.friends.length} friends,
 and his best friend is called ${amrObject.friends.at(0)}`);
 
 amrObject.calcAge = function () {
-    // console.log(this);
-    return this.age = 2025 - this.birthYear;
+  // console.log(this);
+  return (this.age = 2025 - this.birthYear);
 };
 
 console.log(amrObject.calcAge());
@@ -95,31 +86,15 @@ console.log(amrObject.age);
 // console.log(amrObject['calcAge'](2000));
 
 amrObject.getSummary = function () {
-    return `${this.firstName} is a ${this.calcAge()}-year old person, and he has ${this.friends.length} friends.`
+  return `${
+    this.firstName
+  } is a ${this.calcAge()}-year old person, and he has ${
+    this.friends.length
+  } friends.`;
 };
 
 console.log(amrObject.getSummary());
 
-
 for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
