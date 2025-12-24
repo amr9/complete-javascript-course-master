ee7557631d0d86d925f5d0f376a75b375f9c87ee
amr9 <amr12201147@gmail.com>
2025-12-24 04:44:01 +0200

    Implement temperature amplitude calculation function and log results

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -9,3 +9,34 @@ console.log("Hello World!");
 // BUG
 // TODO
 // FIXME
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    // debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude([
+  3,
+  -2,
+  -6,
+  -1,
+  "error",
+  9,
+  13,
+  17,
+  15,
+  14,
+  9,
+  5,
+]);
+console.log(amplitude);
