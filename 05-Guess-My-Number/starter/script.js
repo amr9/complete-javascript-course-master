101b56246cf376330f3f400a4c99ba7c5cff9a16
amr9 <amr12201147@gmail.com>
2025-12-27 07:41:25 +0200

    fixing the high score bug

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,6 +12,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -22,7 +23,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.highscore').textContent = score;
+    if (score > highscore) {
+      document.querySelector('.highscore').textContent = score;
+      highscore = score;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high';
