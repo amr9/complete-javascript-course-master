6e2b968238166a1d4a7e75b8a095ab8245f9c6d6
amr9 <amr12201147@gmail.com>
2025-12-27 06:28:17 +0200

    values

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,11 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'correct number';
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
