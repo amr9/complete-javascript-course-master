1f0794435f37605e3c30a901aca9d75696df5cf5
amr9 <amr12201147@gmail.com>
2025-12-28 09:30:47 +0200

    Initialize game state and add event listeners for game controls

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,59 @@
 'use strict';
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const diceImage = document.querySelector('.dice');
+const scoreText0 = document.querySelector('#score--0');
+const scoreText1 = document.querySelector('#score--1');
+const currentScore0 = document.querySelector('#current--0');
+const currentScore1 = document.querySelector('#current--1');
+const playSection0 = document.querySelector('.player--0');
+const playSection1 = document.querySelector('.player--1');
+
+let score = 0;
+let total = 0;
+let activePlayer = 0;
+diceImage.classList.add('hidden');
+
+const clickBtnNew = function () {
+  scoreText0.textContent = '0';
+  scoreText1.textContent = '0';
+  currentScore0.textContent = '0';
+  currentScore1.textContent = '0';
+  playSection0.classList.add('player--active');
+  playSection1.classList.remove('player--active');
+  diceImage.classList.add('hidden');
+  total = 0;
+};
+
+const clickBtnRoll = function () {
+  let random = Math.trunc(Math.random() * 6) + 1;
+  diceImage.src = `dice-${random}.png`;
+  diceImage.classList.remove('hidden');
+  console.log(random);
+  if (random === 1) {
+    document.querySelector(`#score--${activePlayer}`).textContent = '0';
+    score = 0;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    playSection0.classList.toggle('player--active');
+    playSection1.classList.toggle('player--active');
+  } else {
+    score += random;
+    document.querySelector(`#score--${activePlayer}`).textContent = score;
+  }
+};
+
+const clickBtnHold = function () {
+  if (score !== 0) {
+    total += score;
+    document.querySelector(`#current--${activePlayer}`).textContent = total;
+    document.querySelector(`#score--${activePlayer}`).textContent = '0';
+    score = 0;
+    diceImage.classList.add('hidden');
+  }
+};
+
+btnNew.addEventListener('click', clickBtnNew);
+btnRoll.addEventListener('click', clickBtnRoll);
+btnHold.addEventListener('click', clickBtnHold);
