57152da6ea332233272dc7bbf83a4f773bc24888
amr9 <amr12201147@gmail.com>
2026-01-04 03:22:51 +0200

    maps and map methods

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -284,3 +284,64 @@ const uniqueItalianFoodsAndMexicanFoods =
 console.log(uniqueItalianFoodsAndMexicanFoods);
 
 console.log(mexicanFoods.isDisjointFrom(italianFoods));
+
+//maps
+const rest = new Map();
+rest.set('name', 'classico italiano');
+rest.set(1, 'firenze italy');
+console.log(rest.set(2, 'lisbon'));
+
+//chaining maps
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'we are open')
+  .set(false, 'we are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 23;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+
+console.log('deletion of key "2"', rest.delete(2));
+const array12 = [1, 2];
+rest.set(array12, 'test');
+// rest.clear();
+rest.set(document.querySelector('h1'), 'heading');
+console.log(rest);
+
+console.log(rest.get(array12));
+
+const question = new Map([
+  ['question', 'what is the best language?'],
+  [1, 'c'],
+  [2, 'java'],
+  [3, 'javascript'],
+  ['correct', 3],
+  [true, 'correct'],
+  [false, 'try again'],
+]);
+console.log(question);
+//convert object to map
+console.log(Object.entries(openingHours));
+const hoursmap = new Map(Object.entries(openingHours));
+console.log(hoursmap);
+//quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`answer ${key}: ${value}`);
+  }
+}
+
+// const answer = Number(prompt('your answer'));
+// console.log(answer);
+// console.log(question.get(answer === question.get('correct')));
+
+//convert map to array
+console.log(...question);
