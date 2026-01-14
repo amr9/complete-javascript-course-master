bc8b3364e98f974e41b2925183950e0438fc2818
amr9 <amr12201147@gmail.com>
2026-01-14 14:58:26 +0200

    challenge #3

--- 11-Arrays-Bankist/starter/challenges.js
+++ 11-Arrays-Bankist/starter/challenges.js
@@ -109,3 +109,26 @@ const calcAverageHumanAge = function (ages) {
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log('----------------------');
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an
+arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+const calcAverageHumanAgeChain = ages => {
+  let funcarr = [...ages];
+  funcarr = funcarr
+    .map((age, i, arr) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age, i, arr) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return funcarr;
+};
+
+console.log(calcAverageHumanAgeChain([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAgeChain([16, 6, 10, 5, 6, 1, 4]));
