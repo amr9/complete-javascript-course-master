7dbcd0670f966118f21fc6f531920b8bf998d537
amr9 <amr12201147@gmail.com>
2026-01-22 21:03:31 +0200

    fix: correct HTML doctype and add designer attribute to logo; enhance modal functionality

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +34,92 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+console.log(document.documentElement);
+
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//creating and inserting elements
+//.insertAdjacentHtml()
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+// message.textContent = 'We use cookies for improved analytics';
+
+message.innerHTML = `We use cookied for improved functionality and analytics. 
+  <button class="btn btn--close-cookie">Got it!</button>`;
+header.prepend(message); //prepend and append inserts element inside the header where header is the parent
+// header.append(message.cloneNode(true)); //makes multiple copies of html element
+
+header.after(message); //before and after inserts object before and after the header as siblings
+// header.after(message);
+
+//delete element
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+//styles
+message.style.backgroundColor = 'blue';
+
+console.log(message.style.height); //this is bec. message.style find the inline styles only
+
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'wassup wassup';
+
+//non standard
+console.log(logo.desginer);
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'bankist');
+
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+
+console.log(link.href);
+
+console.log(link.getAttribute('href'));
+
+//data attributes
+console.log(logo.dataset.versionNumber);
+
+//classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
