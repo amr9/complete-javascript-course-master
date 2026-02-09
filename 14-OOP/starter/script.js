c7bd2e52908445a801a5d37b9f831782857093a7
amr9 <amr12201147@gmail.com>
2026-02-10 00:23:44 +0200

    feat: implement class-based Person constructor with age calculation and account management

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -64,3 +64,81 @@ console.log(array.unique());
 const h1 = document.querySelector('h1');
 console.dir(h1);
 console.dir(x => x + 1);
+
+const personClEx = class {};
+
+class personCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new personCl('jessica davis', 1996);
+
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === personCl.prototype);
+
+const account = {
+  owner: 'amr',
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+    console.log(this.movements);
+  },
+};
+
+console.log(account.latest);
+account.latest = 900;
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+//when creating class like this it it will be class and prototype
+const steven = Object.create(PersonProto);
+
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+// there is no prototype for personproto bec it is the prototype
+console.log(PersonProto.prototype);
+
+const sarah = Object.create(PersonProto);
+
+sarah.init('Sarah', 1979);
+
+sarah.calcAge();
